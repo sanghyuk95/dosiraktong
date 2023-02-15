1b7e33f532bfb74bbd6d3009b156fcde806c08ce
burny <shskse5@gmail.com>
2023-02-15 18:09:27 +0900

    02-15 슬라이드

--- js/script.js
+++ js/script.js
@@ -1,29 +1,18 @@
 // 이미지 및 리소스 로드 후 코드실행
 window.onload = function () {
   let htmlTag = document.querySelector("html");
-  // 모바일 메뉴 버튼 처리
-  // 1. 모바일 버튼을 찾아서 저장한다.
   let mbBt = document.querySelector(".mb-bt");
-  // 2. 모바일 메뉴를 찾아서 저장한다.
   let mbNav = document.querySelector(".mb-nav");
-  // 3. 로고를 찾아서 저장한다.
   let logo = document.querySelector(".logo");
-  // 4. header 를 찾아서 저장한다.
   let header = document.querySelector(".header");
-  // 5. gnb > li > a
   let gnbA = document.querySelectorAll(".gnb>li>a");
-  // 6. mb-bt span
   let mbBtSpan = document.querySelectorAll(".mb-bt span");
 
   // 4. 모바일 버튼 클릭을 하면
   mbBt.addEventListener("click", function () {
-    // html scroll 없애기
     htmlTag.classList.toggle("active");
-    //  로고에 active 클래스를 추가한다.
     logo.classList.toggle("active-blue");
-    //  모바일 버튼에 active 클래스를 추가한다.
     mbBt.classList.toggle("active");
-    // 모바일 메뉴에 active 클래스를 추가한다.
     mbNav.classList.toggle("active");
     mbBtSpan.forEach((item) => {
       item.classList.toggle("active");
@@ -31,27 +20,18 @@ window.onload = function () {
   });
   // 화면 리사이징 처리
   window.addEventListener("resize", function () {
-    // window 의 화면 안쪽 너비 체크
-    // console.log(window.innerWidth);
     let wW = window.innerWidth;
     if (wW > 1080) {
-      // html scroll 없애기
       htmlTag.classList.remove("active");
-
-      //  모바일 버튼에 active 클래스를 제거한다.
       mbBt.classList.remove("active");
-      // 모바일 메뉴에 active 클래스를 제거한다.
       mbNav.classList.remove("active");
 
-      // 스크롤이 되었는지 안되었는지에 따라서 처리 분리
       let scT = window.document.documentElement.scrollTop;
       if (scT > 100) {
-        // 스크롤이 되었으므로
         mbBtSpan.forEach((item) => {
           item.classList.add("active");
         });
       } else {
-        // 모바일 버튼 아이콘 색상 짙게(#fff)
         mbBtSpan.forEach((item) => {
           item.classList.remove("active");
         });
@@ -62,9 +42,7 @@ window.onload = function () {
   });
   // window 스크롤 처리
   window.addEventListener("scroll", function () {
-    // 스크롤바가 스크롤이 된 픽셀 값을 파악
     let scT = window.document.documentElement.scrollTop;
-    // 조금이라도 스크롤을 했다면 처리한다.
     if (scT > 100) {
       header.classList.add("active");
       logo.classList.add("active");
@@ -95,5 +73,97 @@ window.onload = function () {
     });
   }
 
-  var swiper = new Swiper(".swvisual", {});
+  // data.json 외부연동
+  // 1. XMLHttpRequest 활용(반드시 JSON.parse() 실행)
+  // const xhttp = new XMLHttpRequest();
+  // xhttp.onreadystatechange = (e) => {
+  //   console.log(e.target);
+  //   const req = e.target;
+  //   if (req.readyState === XMLHttpRequest.DONE) {
+  //     console.log(req.response);
+  //     const dataArr = JSON.parse(req.response)
+  //   }
+  // };
+  // xhttp.open("GET", "data.json");
+  // xhttp.send();
+
+  // 2.fetch
+  fetch("data.json")
+    .then((res) => res.json())
+    .then((data) => {
+      // 여기서 넘어온 데이터는 문자열
+      console.log(data.visual);
+      visualData = data.visual;
+      let html = "";
+      let count = 1;
+      visualData.forEach((item) => {
+        html += `<li>${count++}</li>`;
+      });
+      swUl.innerHTML = html;
+      swList = document.querySelectorAll(".swvisual-list li");
+      swListShow()
+      showVT(visualData[0]);
+      changeBar(0);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  // 비주얼에 활용할 데이터
+  let visualData;
+  const swTitle = document.querySelector(".swvisual-title");
+  const swTxt = document.querySelector(".swvisual-txt");
+  const swLink = document.querySelector(".swvisual-link");
+  const swUl = document.querySelector(".swvisual-list");
+  //li 는 데이터 로딩 후 만든다
+  let swList;
+
+  // 타이틀 내용
+  const showVT = (data) => {
+    swTitle.innerHTML = data.title;
+    swTxt.innerHTML = data.txt;
+    if (data.link === "no") {
+      swLink.classList.add("active");
+    } else {
+      swLink.classList.remove("active");
+    }
+  };
+  // 포커스라인 애니메이션
+  const changeBar = (index) => {
+    swList.forEach((item, idx) => {
+      if (idx === index) {
+        item.classList.add("active");
+      } else {
+        item.classList.remove("active");
+      }
+    });
+  };
+
+  const swListShow = () => {
+    swList.forEach((item,idx) => {
+      item.addEventListener('click', () => {
+        swVisual.slideToLoop(idx,500,false)
+      })
+    })
+  }
+
+  let swVisual = new Swiper(".swvisual", {
+    effect: "fade",
+    loop: true,
+    spped: 1000,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      prevEl: ".swvisual-prev",
+      nextEl: ".swvisual-next",
+    },
+  });
+
+  swVisual.on("slideChange", () => {
+    console.log(`진짜 순서 : ${swVisual.realIndex}`);
+    showVT(visualData[swVisual.realIndex]);
+    changeBar(swVisual.realIndex);
+  });
 };
